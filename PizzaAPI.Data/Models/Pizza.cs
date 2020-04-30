0cb25d1ada038adf514529eec7c3c0e6ade29ed2
= <=>
2020-04-30 15:25:16 -0500

    implemented add pizza in controller

--- PizzaAPI.Data/Models/Pizza.cs
+++ PizzaAPI.Data/Models/Pizza.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PizzaAPI.Data.Models
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class Pizza
     {
         public Pizza()
@@ -9,12 +11,17 @@ namespace PizzaAPI.Data.Models
             PizzaOrders = new HashSet<PizzaOrder>();
         }
 
+        [JsonProperty]
         public short PizzaId { get; set; }
+        [JsonProperty]
         public string Name { get; set; }
+        [JsonProperty]
         public string[] Toppings { get; set; }
+        [JsonProperty]
         public string Crust { get; set; }
+        [JsonProperty]
         public string Sauce { get; set; }
-
+        [JsonProperty]
         public ICollection<PizzaOrder> PizzaOrders { get; set; }
     }
 }
--- PizzaAPI/Controllers/PizzasController.cs
+++ PizzaAPI/Controllers/PizzasController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using PizzaAPI.Data.Models;
 using PizzaAPI.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PizzaAPI.Controllers
 {
@@ -34,17 +38,26 @@ namespace PizzaAPI.Controllers
         }
 
         [HttpPost("[action]")]
-        public IActionResult AddPizza()
+        public async Task<IActionResult> AddPizza()
         {
             try
             {
-                // TODO
+                using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+
+                var requestBodyAsJsonString = await reader.ReadToEndAsync();
+
+                if (string.IsNullOrWhiteSpace(requestBodyAsJsonString))
+                    throw new ArgumentException("No request body was given to AddPizza");
+
+                var pizza = JsonConvert.DeserializeObject<Pizza>(requestBodyAsJsonString);
+
+                _pizzasService.AddPizza(pizza);
 
                 return Ok();
             }
-            catch
+            catch (Exception ex)
             {
-                // TOOD: Log
+                _logger.LogError($"An error occurred in AddPizza: {ex.Message}");
                 return BadRequest();
             }
         }
@@ -58,9 +71,9 @@ namespace PizzaAPI.Controllers
 
                 return Ok();
             }
-            catch
+            catch (Exception ex)
             {
-                // TOOD: Log
+                _logger.LogError($"An error occurred in UpdatePizza for ID {id}: {ex.Message}");
                 return BadRequest();
             }
         }
@@ -74,9 +87,9 @@ namespace PizzaAPI.Controllers
 
                 return Ok();
             }
-            catch
+            catch (Exception ex)
             {
-                // TOOD: Log
+                _logger.LogError($"An error occurred in DeletePizza for ID {id}: {ex.Message}");
                 return BadRequest();
             }
         }
