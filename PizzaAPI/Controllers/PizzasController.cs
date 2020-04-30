82e53949dcd8e817ec7a59083310cbfa304faa04
= <=>
2020-04-30 13:33:18 -0500

    added Get Pizzas

--- PizzaAPI/Controllers/PizzasController.cs
+++ PizzaAPI/Controllers/PizzasController.cs
@@ -1,15 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzaAPI.Data.Models;
+using PizzaAPI.Services;
+using System;
+using System.Collections.Generic;
 
 namespace PizzaAPI.Controllers
 {
     [Route("[controller]")]
     public class PizzasController : ControllerBase
     {
-        [HttpGet("[action]")]
-        public IActionResult GetPizzas()
+        private readonly IPizzasService _pizzasService;
+        private readonly ILogger<PizzasController> _logger;
+
+        public PizzasController(IPizzasService pizzasService, ILogger<PizzasController> logger)
         {
-            return Ok("Hello World");
+            _pizzasService = pizzasService;
+            _logger = logger;
         }
 
+        [HttpGet("[action]")]
+        public List<Pizza> GetPizzas()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log
+                return new List<Pizza>();
+            }
+        }
     }
 }
